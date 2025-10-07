381a7760bed79e715d8b5d26ae49c292530c1a06
Madss Calls <madscalls@gmail.com>
2025-10-07 12:29:57 -0400

    entry level

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../blocks/App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+      </div>
+    </div>
+  );
+}
+export default App;
