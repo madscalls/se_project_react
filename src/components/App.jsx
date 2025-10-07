5198103e629028d3369acae3db5fee27aebe3ae3
Madss Calls <madscalls@gmail.com>
2025-10-07 15:01:25 -0400

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import "../blocks/App.css";
+import Header from "../components/Header/Header";
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import "../Header/header.css";
+import logo from "../../images/Logo.svg";
+import avatar from "../../images/avatar.svg";
+
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">NAME</p>{" "}
+        <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
