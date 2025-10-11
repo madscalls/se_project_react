c76a39c218cb414a1bd12cf96d9b7692e7637e83
Madss Calls <madscalls@gmail.com>
2025-10-10 23:10:51 -0400

    first submission

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,14 +1,14 @@
 import "./ItemModal.css";
-import closebtn from "../../images/close-btn.svg";
+import closebtnWhite from "../../images/close-btn-WHITE.svg";
 
 const ItemModal = ({ onClose, card }) => {
   return (
     <div className="modal modal__opened">
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
-          <img className="modal__close-btn" alt="close" src={closebtn} />
+          <img className="modal__close-btn" alt="close" src={closebtnWhite} />
         </button>
-        <img src={card.link} alt="" className="modal__image" />
+        <img src={card.link} alt={card.name} className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather} </p>
