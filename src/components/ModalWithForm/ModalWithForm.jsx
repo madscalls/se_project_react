74353365bcc5c22ecffd05fc449c5a557fe10b14
Madss Calls <madscalls@gmail.com>
2025-10-12 19:45:20 -0400

    second submission

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -4,20 +4,18 @@ import closebtn from "../../images/close-btn.svg";
 function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div className="modal modal__opened">
-      <div className="modal__content">
-        <div>
-          <h2 className="modal__title">{title}</h2>
-          <button onClick={onClose} type="button" className="modal__close">
-            <img className="modal__close-btn" alt="close" src={closebtn} />
-          </button>
-          <form className="modal__form">
-            {children}
+      <div className="modal__content-form">
+        <h2 className="modal__title">{title}</h2>
+        <button onClick={onClose} type="button" className="modal__close">
+          <img className="modal__close-btn" alt="close" src={closebtn} />
+        </button>
+        <form className="modal__form">
+          {children}
 
-            <button className="modal__submit" type="submit">
-              {buttonText}{" "}
-            </button>
-          </form>
-        </div>
+          <button className="modal__submit" type="submit">
+            {buttonText}{" "}
+          </button>
+        </form>
       </div>
     </div>
   );
