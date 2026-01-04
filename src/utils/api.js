bfbbfcbe21b1848fcb92c4191528459435bbab8f
Madss Calls <madscalls@gmail.com>
2026-01-04 08:51:42 -0500

    frontend deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.mcallahanse.jumpingcrab.com"
+    : "http://localhost:3001";
 
 export const handleServerResponse = (res) =>
   res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.mcallahanse.jumpingcrab.com"
+    : "http://localhost:3001";
 import { handleServerResponse } from "./api";
 
 export const signUp = ({ name, avatar, email, password }) => {
